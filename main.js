e09bf3ef8d72def26d7918eee6dbffb626b3825a
stefan lazu <lazustefan@gmail.com>
2021-11-22 23:37:17 +0200

    primul javascript

---
+++ main.js
@@ -0,0 +1,28 @@
+let person = 'Jane Smith'
+let amount = '$12,450'
+let valuation = 'is a lot of money'
+let car = 'a Toyota Yaris'
+let intention = 'plans to buy'
+let action = 'will use her'
+let deposit = 'her bank account'
+let statement = '$12,450 is a lot of money'
+console.log(person + ' has ' + amount + ' in ' + deposit +'.')
+console.log(statement + '.')
+console.log(person + ' ' + intention + ' ' + car + '.')
+intention = 'to buy'
+console.log(person + ' will use her ' + amount + ' ' + intention + ' ' + car +'.')
+console.log(statement + ' for ' + car + '.')
+console.log(" \n=================================================================\n ")
+console.log(statement + '.')
+console.log(statement + " for " + car + '.')
+intention = 'plans to buy'
+console.log(person + ' ' + intention + ' ' + car + '.')
+console.log(person + ' has ' +  amount + ' in ' + deposit +'.')
+intention = 'to buy'
+console.log(person + '  ' + action + ' ' + amount + ' ' + intention + ' ' + car +'.')
+console.log(" \n=================================================================\n ")
+console.log(person + ' ' + intention + ' ' + car + '.')
+console.log(statement + " for " + car + '.')
+console.log(person + ' ' + action + ' ' + amount + ' ' + car +'.')
+console.log(person + ' has ' + amount + ' in ' + deposit +'.')
+console.log(statement + '.')
