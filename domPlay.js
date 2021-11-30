22376b96df78fbc0eb87893a2b333ef93b5bbc6a
stefan lazu <lazustefan@gmail.com>
2021-11-30 16:28:29 +0200

    domPlay curs 5

---
+++ domPlay.js
@@ -0,0 +1,9 @@
+let moveButtons = document.querySelectorAll('#popup .arrowMove');
+
+let ourMoveButton = moveButtons[0];
+
+ourMoveeButton.addEventListener('click', function () {
+
+    let ourPopup = document.getElementById('popup');
+    ourPopup.style.display = 'block';
+});
