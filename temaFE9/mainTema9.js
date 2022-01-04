94c73c28e0095e330b68226256443d9015f07501
Stefan L <lazustefan@gmail.com>
2022-01-04 16:38:41 +0200

    tema9 final

--- temaFE9/mainTema9.js
+++ temaFE9/mainTema9.js
@@ -1,19 +1,23 @@
-let selectieLansari = function () {
-    let despreLansari = function(response) {
-        let detaliiLansari = response.flight_number
-        let 1Lansare = detaliiLansari[0]
-        let 2Lansare = detaliiLansari[1]
-        let 3Lansare = detaliiLansari[2]
-        let dateLansari = document.getElementById("tabelLansari");
-        let row = dateLansari.insertRow()
+let despreLansari = function(response) {
+    let detaliiLansari = response.data;
+    let tabelLansari = document.getElementById("tabelLansari");
+    for (let i = 0; i < detaliiLansari.length ; i++) {
+        let row = tabelLansari.insertRow()
         let cell1 = row.insertCell();
         let cell2 = row.insertCell();
         let cell3 = row.insertCell();
-        cell1.innerHTML = 1Lansare
-        cell2.innerHTML = 2Lansare
-        cell3.innerHTML = 3Lansare
+        let cell4 = row.insertCell();
+        let id = detaliiLansari[i].id;
+        let flightNumber  = detaliiLansari[i].flight_number;
+        let ships = detaliiLansari[i].ships.length;
+        let date = detaliiLansari[i].date_utc;
+        cell1.innerHTML = id
+        cell2.innerHTML = flightNumber
+        cell3.innerHTML = ships
+        cell4.innerHTML = date
     }
-   axios.get('https://api.spacexdata.com/v4/launches').then(despreLansari);
 }
+axios.get('https://api.spacexdata.com/v4/launches').then(despreLansari);
+
 
 
